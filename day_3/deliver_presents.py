d1997d04b536288bb5d8826d89a20d7f2c9443a7
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-17 18:03:17 -0500

    add robo-santa

--- day_3/deliver_presents.py
+++ day_3/deliver_presents.py
@@ -6,22 +6,27 @@ elfs_drunken_directions = ""
 for line in fileinput.input(filename):
     elfs_drunken_directions += line
 
-x = 0
-y = 0
-happy_children = {(x, y): 1}
+santa = {'x': 0, 'y': 0}
+robo = {'x': 0, 'y': 0}
+happy_children = {(0, 0): 1}
+gifter = santa
 
 for drunk_direction in elfs_drunken_directions:
     if drunk_direction == '^':
-        y += 1
+        gifter['y'] += 1
     elif drunk_direction == '>':
-        x += 1
+        gifter['x'] += 1
     elif drunk_direction == 'v':
-        y -= 1
+        gifter['y'] -= 1
     elif drunk_direction == '<':
-        x -= 1
+        gifter['x'] -= 1
 
-    gps = (x, y)
+    gps = (gifter['x'], gifter['y'])
     happy_children[gps] = 1
 
+    if gifter == santa:
+        gifter = robo
+    else:
+        gifter = santa
 
 print len(happy_children.keys())
