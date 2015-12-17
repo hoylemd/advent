1ee9597c1e277364bc2464674ccb6fe734358c7d
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-17 17:11:02 -0500

    add scaffolding

--- day_3/deliver_presents.py
+++ day_3/deliver_presents.py
@@ -1,6 +1,19 @@
 import fileinput
 
-filename = 'input.txt'
+filename = 'test.txt'
+# filename = 'input.txt'
 
+elfs_drunken_directions = ""
 for line in fileinput.input(filename):
-    pass
+    elfs_drunken_directions += line
+
+x = 0
+y = 0
+happy_children = {}
+
+for drunk_direction in elfs_drunken_directions:
+    gps = (x, y)
+    happy_children[gps] = 1
+
+
+print len(happy_children.keys())
