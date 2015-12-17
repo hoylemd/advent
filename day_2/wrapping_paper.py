3eb8e4a505d9ecdf6c62f4ed9035622d19ee9084
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-17 13:34:16 -0500

    implement my solution
    pretty simple - calculate area of dies, find the smallest, calc surace
    area + smallest side area, sum it all.

--- day_2/wrapping_paper.py
+++ day_2/wrapping_paper.py
@@ -1,7 +1,23 @@
 import fileinput
 
 filename = 'input.txt'
-lines = []
 
+def calc_paper(length, width, height):
+    length = int(length)
+    width = int(width)
+    height = int(height)
+
+    front = height * width
+    side = height * length
+    top = length * width
+
+    extra = min([front, side, top])
+
+    return extra + 2 * front + 2 * side + 2 * top
+
+paper = 0
 for line in fileinput.input(filename):
-    lines.append(line)
+    dimensions = line.split('x')
+    paper += calc_paper(dimensions[0], dimensions[1], dimensions[2])
+
+print paper
