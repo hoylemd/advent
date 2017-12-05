033d7e1afcb37d8dcbf358ad8eb72947497e1433
Michael D. Hoyle <hoylemd@gmail.com>
2017-12-05 15:11:36 -0500

    put old stuff in directory

---
+++ 2015/day_2/day_2/wrapping_materials.py
@@ -0,0 +1,45 @@
+import fileinput
+
+filename = 'input.txt'
+
+
+def calc_materials(dimensions):
+    dimensions = line.split('x')
+    length = int(dimensions[0])
+    width = int(dimensions[1])
+    height = int(dimensions[2])
+
+    totals = {'paper': 0, 'ribbon': 0}
+
+    # calculate side area
+    front = height * width
+    side = height * length
+    top = length * width
+
+    # calculate 'extra paper'
+    extra = min([front, side, top])
+
+    # calculate total paper needed
+    totals['paper'] = extra + 2 * front + 2 * side + 2 * top
+
+    # calculate perimeters
+    front_perimeter = 2 * height + 2 * width
+    side_perimeter = 2 * height + 2 * length
+    top_perimeter = 2 * length + 2 * width
+
+    # calculate wrapping ribbon needed
+    totals['ribbon'] = min([front_perimeter, side_perimeter, top_perimeter])
+
+    # calculate volume
+    totals['ribbon'] += length * width * height
+
+    return totals
+
+paper = 0
+ribbon = 0
+for line in fileinput.input(filename):
+    materials = calc_materials(line)
+    paper += materials['paper']
+    ribbon += materials['ribbon']
+
+print ribbon
---
+++ 2015/day_3/day_3/deliver_presents.py
@@ -0,0 +1,32 @@
+import fileinput
+
+filename = 'input.txt'
+
+elfs_drunken_directions = ""
+for line in fileinput.input(filename):
+    elfs_drunken_directions += line
+
+santa = {'x': 0, 'y': 0}
+robo = {'x': 0, 'y': 0}
+happy_children = {(0, 0): 1}
+gifter = santa
+
+for drunk_direction in elfs_drunken_directions:
+    if drunk_direction == '^':
+        gifter['y'] += 1
+    elif drunk_direction == '>':
+        gifter['x'] += 1
+    elif drunk_direction == 'v':
+        gifter['y'] -= 1
+    elif drunk_direction == '<':
+        gifter['x'] -= 1
+
+    gps = (gifter['x'], gifter['y'])
+    happy_children[gps] = 1
+
+    if gifter == santa:
+        gifter = robo
+    else:
+        gifter = santa
+
+print len(happy_children.keys())
