6ae4d154c88e7759376ff27fcb7c143357f0e0c2
Michael D. Hoyle <hoylemd@gmail.com>
2017-12-05 15:36:53 -0500

    add part 2 tests

--- 2017/1/test.py
+++ 2017/1/test.py
@@ -1,7 +1,7 @@
 from solve_captcha import sum_matches
 
 
-def main():
+def test_next():
     cases = {
         '1122': 3,
         '1111': 4,
@@ -13,6 +13,26 @@ def main():
         result = sum_matches(string) == result
         print('{case} {passfail}'
               .format(case=string, passfail='Passed' if result else 'Failed'))
+
+
+def test_opposite():
+    cases = {
+        '1212': 6,
+        '1221': 0,
+        '123425': 4,
+        '123123': 12,
+        '12131515': 4,
+    }
+
+    for string, result in cases.items():
+        result = sum_matches(string, opposite=True) == result
+        print('{case} {passfail}'
+              .format(case=string, passfail='Passed' if result else 'Failed'))
+
+
+def main():
+    test_next()
+    test_opposite()
 
 
 if __name__ == '__main__':
