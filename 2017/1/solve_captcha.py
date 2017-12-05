28cf9a6df60dd6ea474dbf65dd4615c5e2e126ea
Michael D. Hoyle <hoylemd@gmail.com>
2017-12-05 15:29:11 -0500

    solve the puzzle!

--- 2017/1/solve_captcha.py
+++ 2017/1/solve_captcha.py
@@ -6,7 +6,22 @@ parser.add_argument('input_string', type=str, help='The input string')
 
 def main():
     args = parser.parse_args()
-    print(args.input_string)
+    print(sum_matches(args.input_string))
+
+
+def sum_matches(string):
+    total = 0
+    for i in range(len(string)):
+        current = string[i]
+        try:
+            next = string[i + 1]
+        except IndexError:
+            next = string[0]
+
+        if current == next:
+            total += int(current)
+
+    return total
 
 
 if __name__ == '__main__':
