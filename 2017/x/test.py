9d57792ae6f4d30454f1af13ed7b5b0d6d6d3eb8
Michael D. Hoyle <hoylemd@gmail.com>
2017-12-07 12:04:45 -0500

    rejig test runner a bit

--- 2017/x/test.py
+++ 2017/x/test.py
@@ -1,23 +1,28 @@
 from solve import solve
 
 
-def io_test(*args, **kwargs):
-    result = solve(*args, **kwargs)
+def io_test(*arguments):
+    result = solve(*arguments)
 
     return result
 
 
 tests = {
-    'test_center': (io_test, 0, [1], None),
-    'test_adjecent': (io_test, 1, [4], None),
-    'test_side': (io_test, 3, [12], None),
-    'test_corner': (io_test, 4, [13], None),
-    'test_last': (io_test, 6, [49], None),
-    'test_first': (io_test, 3, [10], None)
+    'example': (sorted, ['one', 'two', 'three'], ['one', 'three', 'two']),
+}
+
+test_cases = {
+    sorted: (
+        ("hello", ['e', 'h', 'l', 'l', 'o'],),
+        ([2, -3, 12, 1], [-3, 1, 2, 12],),
+    )
 }
 
 
-def compose_result(result, expected):
+def compose_result(result, expected=None):
+    if expected is None:
+        expected = True
+
     if result == expected:
         return 'Pass'
 
@@ -27,16 +32,47 @@ def compose_result(result, expected):
     )
 
 
-def main():
-    for label, spec in tests.items():
-        test, expected, args, kwargs = spec
-        args = args or []
-        kwargs = kwargs or {}
+def _is_str_or_noniterable(obj):
+    if isinstance(obj, str):
+        return True
 
-        print('{}: {}'.format(
-            label,
-            compose_result(test(*args, **kwargs), expected)
-        ))
+    try:
+        iter(obj)
+    except TypeError:
+        return True
+
+    return False
+
+
+def execute_test(label, test, expected=None, arguments=[]):
+    if not isinstance(arguments, tuple):
+        arguments = (arguments,)
+
+    return '{}: {}'.format(
+        label,
+        compose_result(test(*arguments), expected)
+    )
+
+
+def run_tests(tests):
+    for label, spec in tests.items():
+        test, arguments, expected = spec
+        arguments = arguments or tuple()
+
+        print(execute_test(label, test, expected, arguments))
+
+
+def run_test_cases(test_cases):
+    for test, cases in test_cases.items():
+        for arguments, expected in cases:
+            label = '{}({})'.format(test.__name__, arguments)
+
+            print(execute_test(label, test, expected, arguments))
+
+
+def main():
+    run_tests(tests)
+    run_test_cases(test_cases)
 
 
 if __name__ == '__main__':
