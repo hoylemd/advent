72170479f29835ec2fc67afefcbc8e2331775be5
Michael D. Hoyle <hoylemd@gmail.com>
2017-12-08 00:14:17 -0500

    I'm an IDIOT (redoing day 3)

--- 2017/3/test.py
+++ 2017/3/test.py
@@ -1,56 +1,58 @@
-from solve import solve
+# from solve import solve
+# Switch comments to turn on
+solve = sorted
 
-
-def io_test(*args, **kwargs):
-    result = solve(*args, **kwargs)
-
-    return result
-
-
-def test_sample():
-    result = solve(6)
-
-    return result
-
-
-def count_it_up(max=50, min=1):
-    print('address | distance')
-    print('--------|---------')
-    for i in range(min, max):
-        print('{:7^} | {}'.format(i, solve(i)))
-
-
-tests = {
-    'test_center': (io_test, 0, [1], None),
-    'test_adjecent': (io_test, 1, [4], None),
-    'test_side': (io_test, 3, [12], None),
-    'test_corner': (io_test, 4, [13], None),
-    'test_last': (io_test, 6, [49], None),
-    'test_first': (io_test, 3, [10], None)
-    # 'count_em_up': (count_it_up, None, None, None),
+test_cases = {
+    solve: (
+        ("hello", ['e', 'h', 'l', 'l', 'o'],),
+        ([2, -3, 12, 11], [-3, 1, 2, 12],),
+    )
 }
 
 
-def compose_result(result, expected):
-    if result == expected:
-        return 'Pass'
-
-    return (
-        'Fail: actual: {result}, expected: {expected}'
+def compose_result(outcome, result, expected=None):
+    """Translate test outputs into result message"""
+    return 'Pass' if outcome else (
+        "Fail: act: {result}, exp: {expected}"
         .format(result=result, expected=expected)
     )
 
 
-def main():
-    for label, spec in tests.items():
-        test, expected, args, kwargs = spec
-        args = args or []
-        kwargs = kwargs or {}
+def execute_test(label, test, expected=None, *args):
+    """Runs the test method, and formats a test report"""
 
-        print('{}: {}'.format(
-            label,
-            compose_result(test(*args, **kwargs), expected)
-        ))
+    result = test(*args)
+    outcome = False
+
+    if expected is True and result:
+        outcome = True
+    elif result == expected:
+        outcome = True
+
+    return '{}: {}'.format(
+        label,
+        compose_result(outcome, result, expected),
+    )
+
+
+def run_tests(tests):
+    for label, spec in tests.items():
+        test, arguments, expected = spec
+        arguments = arguments or tuple()
+
+        print(execute_test(label, test, expected, arguments))
+
+
+def run_test_cases(test_cases):
+    for test, cases in test_cases.items():
+        for arguments, expected in cases:
+            label = '{}({})'.format(test.__name__, arguments)
+
+            print(execute_test(label, test, expected, arguments))
+
+
+def main():
+    run_test_cases(test_cases)
 
 
 if __name__ == '__main__':
