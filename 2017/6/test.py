00704760ea6e1deb3b43dea60756b8c8b35b6582
Michael D. Hoyle <hoylemd@gmail.com>
2017-12-13 21:32:54 -0500

    update tests

--- 2017/6/test.py
+++
@@ -1,7 +0,0 @@
-from main import realloc
-
-
-def test_solve():
-    initial_bank = [0, 2, 7, 0]
-
-    assert realloc(initial_bank) == 5
--- 2017/6/test_solve.py
+++ 2017/6/test_solve.py
@@ -1,5 +1,7 @@
-from solve import solve
+from main import realloc
 
 
 def test_solve():
-    assert solve(5) == 7
+    initial_bank = [0, 2, 7, 0]
+
+    assert realloc(initial_bank) == 5
