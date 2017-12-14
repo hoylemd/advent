4009ba8f57a7bdff1a634d71f5b0c3437e8cb088
Michael D. Hoyle <hoylemd@gmail.com>
2017-12-14 09:20:48 -0500

    start on 7

--- 2017/7/main.py
+++ 2017/7/main.py
@@ -1,6 +1,37 @@
 #!/usr/bin/env python
 
 
+
+# Instead of this:
+class Program(Stud):
+    """A program, who may have sub-processes
+
+    :attrs: name, weight, subprocesses, parent, if_sleeping)
+    :flags: sleeping, sudo
+
+    :attr str name: The program's name.
+    :attr int weight: The program's priority weight, default: `50`
+    :attr list subprocesses: `Process`es this process has spawned'
+    :attr `Program` parent: `Process` that spawned this one
+    """
+    def __init__(
+        self,
+        name,
+        weight=50,
+        subprocesses=None,
+        parent=None,
+    ):
+        self.name = name
+        self.weight = weight
+        self.subprocesses = subprocesses or []
+        self.parent = parent
+
+
+def find_root(shouts):
+    """Find the root by tracing the shouts"""
+
+
+
 first_puzzle = len
 second_puzzle = first_puzzle
 
--- 2017/7/test_me.py
+++ 2017/7/test_me.py
@@ -2,10 +2,24 @@ from main import first_puzzle, second_puzzle
 
 
 def test_first():
-    """Should return length 5"""
-    example = 'hello'
+    """Should return length 'tknk'"""
+    example = (
+        'pbga (66)',
+        'xhth (57)',
+        'ebii (61)',
+        'havc (66)',
+        'ktlj (57)',
+        'fwft (72) -> ktlj, cntj, xhth',
+        'qoyq (66)',
+        'padx (45) -> pbga, havc, qoyq',
+        'tknk (41) -> ugml, padx, fwft',
+        'jptl (61)',
+        'ugml (68) -> gyxo, ebii, jptl',
+        'gyxo (61)',
+        'cntj (57)',
+    )
 
-    assert first_puzzle(example) == 5
+    assert first_puzzle(example) == 'tknk'
 
 
 def test_second():
