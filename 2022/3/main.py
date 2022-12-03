b6933b2cb9d0e4cab5d9538b141aa8c4888ab4eb
Mike Hoyle <hoylemd@gmail.com>
2022-12-03 16:21:57 -0500

    solution for 2022-12-03 part 1

--- 2022/3/main.py
+++ 2022/3/main.py
@@ -1,10 +1,37 @@
 import fileinput
 
 def parse_input():
-    return (line.strip().split(' ') for line in fileinput.input())
+    return (line.strip() for line in fileinput.input())
+
+
+def find_dupe(line):
+    """find the duplicate item"""
+    # split in half
+    radix = int(len(line) / 2)
+    first, second = line[0:radix], line[radix:]
+    # print(f"{line}")
+    # print(f"{first} {second}")
+    # find the intersect
+    return set(first).intersection(set(second)).pop()
+
+
+def prioritize(item):
+    """calculate priority of item"""
+    offset = 96  # ascii code for 'a'
+    ascii_val = ord(item)
+
+    if ascii_val < offset:  # it's capital
+        offset = 38 # -64 to nullify ascii offset, +26 to be above lcase = 38
+
+    return ascii_val - offset
 
 if __name__ == '__main__':
     lines = parse_input()
 
+    sum = 0
     for line in lines:
-        print(line)
+        dupe = find_dupe(line)
+        sum += prioritize(dupe)
+
+    print(sum)
+        
