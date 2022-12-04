82e664011841fb39691f09ed71a540a6b3875519
Mike Hoyle <hoylemd@gmail.com>
2022-12-04 11:33:42 -0500

    first (failed) attempt at day 4

--- 2022/4/main.py
+++ 2022/4/main.py
@@ -2,11 +2,31 @@ import fileinput
 
 
 def parse_input():
-    return (line.strip() for line in fileinput.input())
+    return (line.strip().split(',') for line in fileinput.input())
+
+
+def check_bounds(outer, inner):
+    return outer[0] <= inner[0] and outer[1] >= inner[1]
+
+
+def do_overlap(first, second):
+    f_bounds = first.split('-')
+    s_bounds = second.split('-')
+
+    if check_bounds(f_bounds, s_bounds) or check_bounds(s_bounds, f_bounds):
+        return True
 
 
 if __name__ == '__main__':
     lines = parse_input()
 
+    overlaps = 0
     for line in lines:
-        print(line)
+        delta = 1 if do_overlap(*line) else 0
+
+        print(f"{line}{' - overlap' if delta else ''}")
+
+        overlaps += delta
+
+    print()
+    print(overlaps)
