ce4fabfb1c2122d502d9f4376bff51dcb69dac94
Mike Hoyle <hoylemd@gmail.com>
2022-12-06 14:31:23 -0500

    2022-12-06 part 1 sol'n

--- 2022/6/main.py
+++ 2022/6/main.py
@@ -9,11 +9,31 @@ def debug(*args):
 
 
 def parse_input():
-    return (line.strip() for line in fileinput.input())
+    return ''.join(line.strip() for line in fileinput.input())  # one-line input
+
+
+def is_sopm(frame):
+    """given string check if all charsdifferent"""
+    yesno = len(set(frame)) == len(frame)
+
+    debug(f"checking frame: {frame} {yesno}")
+    return yesno
+
+
+
+def find_next_packet(stream):
+    i = 3
+    buffer = ''
+
+    while i < len(stream):
+        if is_sopm(stream[i-3:i+1]):
+            break
+        i += 1
+
+    return i + 1
 
 
 if __name__ == '__main__':
-    lines = parse_input()
+    stream = parse_input()
 
-    for line in lines:
-        debug(line)
+    print(find_next_packet(stream))
