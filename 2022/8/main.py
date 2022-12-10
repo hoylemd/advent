fb38bd7e2b0644e2aa215ea901031d2ba9a49e91
Mike Hoyle <hoylemd@gmail.com>
2022-12-10 18:26:18 -0500

    2022-12-08 part 2 soln

--- 2022/8/main.py
+++ 2022/8/main.py
@@ -16,17 +16,24 @@ class TreeGrid:
     def __init__(self):
         self.grid = []  #[parse_line(line) for line in fileinput.input()]
         self.ext_vis_map = []
+        self.scenic_map = []
+        self.width = 0
+        self.height = 0
+        self.max_scenic = 0
         for line in fileinput.input():
             grid_line = []
             vis_line = []
+            scenic_line = []
             for c in line.strip():
+                if self.height == 0: self.width += 1
                 grid_line.append(int(c))
                 vis_line.append(False)
+                scenic_line.append(0)
+            self.height += 1
+
             self.grid.append(grid_line)
             self.ext_vis_map.append(vis_line)
-
-        self.scan_grid()
-
+            self.scenic_map.append(scenic_line)
 
     def __str__(self):
         return '\n'.join(
@@ -46,14 +53,16 @@ class TreeGrid:
     def vis_grid_line(self, line):
         return f"{''.join(''.join('V' if c else '.' for c in line))}"
 
+    def scenic_grid_line(self, line):
+        return '|'.join(f"{c:2}" for c in line)
+
     def print_grids(self):
         lines = []
 
         for y in range(len(self.grid)):
-            lines.append(f"{self.str_grid_line(self.grid[y])} {self.vis_grid_line(self.ext_vis_map[y])}")
+            lines.append(f"{self.str_grid_line(self.grid[y])} {self.scenic_grid_line(self.scenic_map[y])}")
 
-        debug('\n'.join(lines))
-        debug()
+        return('\n'.join(lines))
 
     def get_tree(self, x, y):
         try:
@@ -65,9 +74,8 @@ class TreeGrid:
         try:
             self.ext_vis_map[y][x] = True
         except TypeError as exc:
-            self.print_grids()
+            debug(self.print_grids())
             debug(f"{x=}, {y=}")
-            breakpoint()
             raise
 
     def scan_line(self, y):
@@ -98,7 +106,6 @@ class TreeGrid:
             if tallest == 9:
                 break  # max height
 
-
     def rotate_grids(self):
         self.grid = list(list(col) for col in zip(*self.grid[::-1]))
         self.ext_vis_map = list(list(col) for col in zip(*self.ext_vis_map[::-1]))
@@ -108,7 +115,7 @@ class TreeGrid:
         for y in range(len(self.grid)):
             self.scan_line(y)
 
-        self.print_grids()
+        debug(self.print_grids())
 
         # rotate grid
         self.rotate_grids()
@@ -117,15 +124,63 @@ class TreeGrid:
         for y in range(len(self.grid)):
             self.scan_line(y)
 
-
     def count_ext_visible(self):
         # count up the ext_vis_map
         return sum(line.count(True) for line in self.ext_vis_map)
 
+    def calc_scenic_score(self, x, y):
+        height = self.get_tree(x, y)
+        debug(f"calc scenic for ({x}, {y}): {height}")
+
+        # scan left
+        l_dist = 0
+        for i in range(x-1, -1, -1):
+            l_dist += 1
+            if self.get_tree(i, y) >= height:
+                break
+        debug(f"{l_dist=}")
+
+        # scan up
+        u_dist = 0
+        for i in range(y-1, -1, -1):
+            u_dist += 1
+            if self.get_tree(x, i) >= height:
+                break
+        debug(f"{u_dist=}")
+
+        # scan right
+        r_dist = 0
+        for i in range(x+1, self.width, 1):
+            r_dist += 1
+            if self.get_tree(i, y) >= height:
+                break
+        debug(f"{r_dist=}")
+
+        # scan down
+        d_dist = 0
+        for i in range(y+1, self.height, 1):
+            d_dist += 1
+            if self.get_tree(x, i) >= height:
+                break
+        debug(f"{d_dist=}")
+
+        score = l_dist * u_dist * r_dist * d_dist
+        self.scenic_map[y][x] = score
+        return score
+
+    def scan_scenic(self):
+        max_scenic = 0
+        for y in range(1, len(self.grid) - 1):
+            for x in range(1, len(self.grid[y]) - 1):
+                score = self.calc_scenic_score(x, y)
+                if score > max_scenic: max_scenic = score
+                debug(f"score for ({x},{y}): {score}{'*' if score == max_scenic else ''}")
+        return max_scenic
 
 if __name__ == '__main__':
     grid = TreeGrid()
 
-    grid.print_grids()
+    print(grid.scan_scenic())
+    #print(grid.calc_scenic_score(2, 3))
 
-    print(grid.count_ext_visible())
+    print(grid.print_grids())
