8a18b6dcfb551d3b87d919983ab2435a39e8d7ea
Mike Hoyle <hoylemd@gmail.com>
2022-12-12 00:56:21 -0500

    2022-12-11 part 2

--- 2022/11/main.py
+++ 2022/11/main.py
@@ -1,11 +1,18 @@
 import fileinput
 
-DEBUG = True
+DEBUG = False  # True
+INFO = False  # True
+
+
+def info(*args):
+    if INFO: print(*args)
 
 
 def debug(*args):
-    if DEBUG:
-        print(*args)
+    if DEBUG: print(*args)
+
+# TODO add basic logging
+# TODO autopep8
 
 
 def parse_input():
@@ -24,6 +31,8 @@ class Monkey:
         self.false_targ = false_targ
 
         self.inspections = 0
+        self.t_insps = 0
+        self.f_insps = 0
 
     def __str__(self):
         if self.op == '**':
@@ -40,20 +49,65 @@ class Monkey:
             f"    If false: throw to monkey {self.false_targ}"
         ])
 
+    def perform_op(self, item):
+        match self.op:
+            case '+':
+                worry = item + self.arg
+                debug(f"    Worry level increases by {self.arg} to {worry}")
+            case '*':
+                worry = item * self.arg
+                debug(f"    Worry level is multiplied by {self.arg} to {worry}")
+            case '**':
+                worry = item ** 2
+                debug(f"    Worry level is multiplied by itself to {worry}")
+
+        return worry
+
+    def inspect(self, item):
+        self.inspections += 1
+        debug(f"  Monkey inspects an item with a worry level of {item}.")
+
+        return self.perform_op(item)
+
+    def throw(self, item):
+        if item % self.div == 0:
+            self.t_insps += 1
+            targ = self.true_targ
+            div_state = ''
+        else:
+            self.f_insps += 1
+            targ = self.false_targ
+            div_state = ' not'
+
+        debug(f"    Current worry level is{div_state} divisible by {self.div}.")
+        debug(f"    Item with worry level {item} is thrown to monkey {targ}.")
+        self.game.monkeys[targ].items.append(item)
+
+    def take_turn(self):
+        for item in self.items:
+            worry = self.inspect(item)
+            proj = worry % self.game.worry_limit
+            if (worry != proj): info(f"    Worry Limit Exceeded ({worry}%{self.game.worry_limit}={proj}")
+            self.throw(proj)
+
+        self.items = []
+
 
 class Game:
     def __init__(self, notes):
         self.monkeys = []
         buffer = []
+        self.worry_limit = 1
         for note in notes:
             if not note:
-                self.monkeys.append(Monkey(*self.parse_monkey(buffer)))
+                self.parse_monkey(buffer)
                 buffer = []
                 continue
             buffer.append(note)
 
         # and get the last one
-        self.monkeys.append(Monkey(*self.parse_monkey(buffer)))
+        self.parse_monkey(buffer)
+        info(f"Worry limit is {self.worry_limit}({'*'.join(str(m.div) for m in self.monkeys)})")
 
     def parse_monkey(self, monkey_spec):
         number = int(monkey_spec[0][-2])
@@ -69,13 +123,45 @@ class Game:
         true_targ = int(monkey_spec[4][25:])
         false_targ = int(monkey_spec[5][26:])
 
-        return self, number, items, op, arg, div, true_targ, false_targ
+        monkey = Monkey(self, number, items, op, arg, div, true_targ, false_targ)
+        self.monkeys.append(monkey)
+        self.worry_limit *= monkey.div
+        return monkey
 
     def __str__(self):
         return '\n\n'.join(str(monkey) for monkey in self.monkeys)
+
+    def sim_round(self):
+        for monkey in self.monkeys:
+            debug(f"Monkey {monkey.number}:")
+            monkey.take_turn()
+
+    def summarize_monkeys(self):
+        lines = (
+            f"  Monkey {m.number} inspected items {m.inspections} times. ({m.t_insps}/{m.f_insps})"
+            f"({','.join(str(i) for i in m.items)})"
+            for m in self.monkeys
+        )
+        return '\n'.join(lines)
+
+    def simulate(self, rounds):
+        for i in range(rounds):
+            info()
+            info(f"Round {i}:    (limit:{self.worry_limit})")
+            self.sim_round()
+            info(self.summarize_monkeys())
+
+    def monkey_business(self):
+        top, sec = sorted([m.inspections for m in self.monkeys])[-1:-3:-1]
+        return top * sec
 
 
 if __name__ == '__main__':
     game = Game(parse_input())
 
-    print(game)
+    rounds = 10_000
+
+    breakpoint()
+    game.simulate(rounds)
+
+    print(game.monkey_business())
