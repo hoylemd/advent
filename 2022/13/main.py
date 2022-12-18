73e7344a79f87e0c3c2cc2adaf810ba952b5e39e
Mike Hoyle <hoylemd@gmail.com>
2022-12-18 16:11:55 -0500

    2022-12-13 part 1 soln

--- 2022/13/main.py
+++ 2022/13/main.py
@@ -16,11 +16,72 @@ def parse_input():
     return (line.strip() for line in fileinput.input())
 
 
+def render_message(message):
+    if isinstance(message, int): return str(message)
+    return json.dumps(message, separators=(',', ':'))
+
+
 def render_packet(*parts):
     return '\n'.join(
-        json.dumps(part, separators=(',', ':'))
+        render_message(part)
         for part in parts
     )
+
+
+def noop():
+    return
+
+
+class WrongOrder(Exception):
+    """Raised when evidence of wrong order found"""
+    pass
+
+
+def compare_lists(left, right, level=1):
+    for i, l_op in enumerate(left):
+        try:
+            r_op = right[i]
+        except IndexError as exc:
+            logger.info(f"{'  ' * level}- Right side ran out of items so inputs are not in the right order")
+            raise WrongOrder() from exc  # right ran out first
+
+        if compare(l_op, r_op, level):
+            return True
+
+    if len(right) > len(left):
+        logger.info(f"{'  ' * level}- Left side ran out of items so inputs are in the right order")
+        return True  # left ran out furst
+
+
+def compare(left, right, level=1):
+    logger.info(f"{'  ' * level}- Compare {render_message(left)} vs {render_message(right)}")
+    if isinstance(left, int) and isinstance(right, int):  # both ints
+        if left < right:
+            logger.info(f"{'  ' * (level + 1)}- Left side is smaller so inputs are in the right order")
+            return True
+        if left > right:
+            logger.info(f"{'  ' * (level + 1)}- Right side is smaller so inputs are not in the right order")
+            raise WrongOrder
+        return False
+
+    if isinstance(left, int):
+        logger.info(f"{'  ' * (level + 1)}- Mixed types; convert left to [{left}] and retry comparison")
+        left = [left]
+    if isinstance(right, int):
+        logger.info(f"{'  ' * (level + 1)}- Mixed types; convert right to [{right}] and retry comparison")
+        right = [right]
+
+    if compare_lists(left, right, level + 1):
+        return True
+
+
+def is_sorted(left, right):
+    logger.info(f"- Compare {render_message(left)} vs {render_message(right)}")
+
+    try:
+        return compare(left, right)
+    except WrongOrder:
+        return False
 
 
 class Thing:
@@ -46,20 +107,31 @@ class Thing:
 
         return packets
 
-    def answer(self, *args, **kwargs):
-        return 0
+    def count_sorted(self):
+        ok_indicies = []
+        for i, packet in enumerate(self.packets):
+            logger.info(f"== Pair {i+1} ==")
+            if is_sorted(*packet):
+                ok_indicies.append(i + 1)
+            logger.info('')
+
+        logger.info(f"Sorted Indicies: {ok_indicies}")
+        return sum(ok_indicies)
+
+    def answer(self, func, *args, **kwargs):
+        return func()
 
     def for_part(self, part='1'):
         return {
-            '1': [],
-            '2': []
+            '1': [self.count_sorted],
+            '2': [noop]
         }[part]
 
 
 if __name__ == '__main__':
     thing = Thing(parse_input())
 
-    logger.info(thing)
+    logger.debug(thing)
     logger.debug('')
 
     print(f"answer:\n{thing.answer(*thing.for_part(environ['ADVENT_PART']))}")
