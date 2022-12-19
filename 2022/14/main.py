587b76e3a7e6e18903656d8146fa98f2002a4995
Mike Hoyle <hoylemd@gmail.com>
2022-12-18 21:13:39 -0500

    start 2022-12-14

---
+++ 2022/14/main.py
@@ -0,0 +1,33 @@
+from os import environ
+from utils import logger, parse_input
+
+
+class Thing:
+    def __init__(self, lines, part):
+        self.part = part
+        self.input = self.parse(lines)
+
+    def __str__(self):
+        return '\n'.join(self.input)
+
+    def parse(self, lines):
+        return [line for line in lines]
+
+    def answer(self, *args, **kwargs):
+        return 0
+
+    def for_part(self, part=1):
+        return {
+            1: [],
+            2: []
+        }[part]
+
+
+if __name__ == '__main__':
+    part = int(environ.get('ADVENT_PART', 1))
+    thing = Thing(parse_input(), part)
+
+    logger.info(thing)
+    logger.debug('')
+
+    print(f"answer:\n{thing.answer(*thing.for_part(part))}")
