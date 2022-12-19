40996805be53f5a76139b3e0d74e83f7edaf7236
Mike Hoyle <hoylemd@gmail.com>
2022-12-19 17:45:27 -0500

    moar work on my Grid class

--- 2022/utils.py
+++ 2022/utils.py
@@ -38,6 +38,14 @@ def num_width(number):
     return width
 
 
+def get_int_char(number, i, width=None):
+    if width is None:
+        rendered = str(number)
+    else:
+        rendered = f"{{:0{width}}}".format(number)
+    return rendered[i]
+
+
 DIR_MAP = {
     'L': (-1, 0),
     'U': (0, 1),
@@ -117,3 +125,134 @@ class Point():
     def unit(self):
         """Reduce this point to a unit vector"""
         return Point(*reduce_vector(*self))
+
+
+class Grid:
+    def __init__(self, dimensions=(0, 0), origin=(0, 0), offset=(0, 0), value=None, x_axis_spacing=5):
+        """Construct the grid
+
+        :param Point dimensions: the width and height of the grid. (0,0) by default
+        :param Point origin: coordinates of the origin. Actual coordinate ranges are
+            defined relative to this point. top-left corner (0,0) by default, see offset below
+        :param Point offset: where the origin is in the grid
+            top-left corner (0,0) by default
+        :param any value: Value to pre-populate the grid with. Won't init_grid if None or omitted
+        :param int x_axis_spacing: show x-axis label every x columns (first, last and origin always labelled)
+        """
+        self.dimensions = Point(*dimensions) if isinstance(dimensions, tuple) else dimensions
+        self.origin = Point(*origin) if isinstance(origin, tuple) else origin
+        self.offset = Point(*offset) if isinstance(offset, tuple) else offset
+
+        self.values = []
+        self.x_axis_spacing = x_axis_spacing
+
+        if value is not None:
+            self.init_grid(value)
+
+    @property
+    def width(self):
+        return self.dimensions.x
+
+    @property
+    def height(self):
+        return self.dimensions.y
+
+    @property
+    def x_bounds(self):
+        lb = self.origin.x - self.offset.x
+        return (lb, lb + self.width - 1)
+
+    @property
+    def y_bounds(self):
+        lb = self.origin.y - self.offset.y
+        return (lb, lb + self.height - 1)
+
+    def init_grid(self, value=None):
+        """actually create the values object"""
+        self.values = [
+            [value] * self.width
+            for _ in range(self.height)
+        ]
+
+    def get(self, x, y=None):
+        if y is None and isinstance(x, Point):
+            x, y = x
+        ax = x - self.x_bounds[0]
+        ay = y - self.y_bounds[0]
+        return self.values[ay][ax]
+
+    def set(self, x, y, value=None):
+        if value is None and isinstance(x, Point):  # unpack point if provided
+            value = y
+            x, y = x
+
+        ax = x - self.x_bounds[0]
+        ay = y - self.y_bounds[0]
+
+        self.values[ay][ax] = value
+
+    def all_values(self):
+        """Depaged iterator to iterate over every cell in the grid"""
+        for x, y in self.enumerate_coords():
+            yield self.get(x, y)
+
+    def all_items(self):
+        """enumerate all coordinates and their values"""
+        for x, y in self.enumerate_coords():
+            yield (x, y, self.get(x, y))
+
+    def enumerate_coords(self):
+        """enumerate all coordinates in the grid"""
+        for y in range(self.y_bounds[0], self.y_bounds[1] + 1):
+            for x in range(self.x_bounds[0], self.x_bounds[1] + 1):
+                yield x, y
+
+    def header_line(self, i, margin, width):
+        spacing = self.x_axis_spacing - 1
+
+        def get_char(val):
+            return get_int_char(val, i, width)
+
+        # first column
+        left = f"{get_char(self.x_bounds[0])}{' ' * (min(spacing, self.x_bounds[1] - self.x_bounds[0]) - 1)}"
+        # inner parts
+        marks = [m + self.x_bounds[0] for m in range(spacing, self.width, self.x_axis_spacing)]
+        inner = (' ' * spacing).join(
+            get_char(j) for j in marks
+        )
+        try:
+            last_mark = marks[-1]
+        except IndexError:
+            last_mark = self.origin.x
+        # last column
+        breakpoint()
+        right = f"{' ' * (self.x_bounds[1] - last_mark - 1)}{get_char(self.x_bounds[1])}"
+
+        line = left + inner + right
+        # add origin mark
+
+        ox = self.origin.x - self.x_bounds[0]
+        if ox not in marks and ox not in self.x_bounds:
+            line = line[:ox] + get_char(self.origin.x) + line[ox + 1:]
+
+        return f"{' ' * margin} {line}"
+
+    def header_lines(self, margin=0):
+        width = max(num_width(self.x_bounds[0]), num_width(self.x_bounds[1]))
+        return [self.header_line(i, margin, width) for i in range(width)]
+
+    def __str__(self):
+        lines = [line for line in render_lines(self.values)]
+        margin = max(num_width(self.y_bounds[0]), num_width(self.y_bounds[1]))
+        stamp = f"{{n:0{margin}}} {{line}}"
+
+        grid_lines = [stamp.format(n=i + self.y_bounds[0], line=line) for i, line in enumerate(lines)]
+
+        return '\n'.join(self.header_lines(margin) + grid_lines)
+
+    def __repr__(self):
+        return f"<Grid object(({self.width},{self.height}),origin:({self.origin}),offset:({self.offset})>"
+
+    def __iter__(self):
+        """iterator of rows, which can be iterated themselves"""
+        return self.values
