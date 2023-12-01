ae9ac345585c833baa03b83838cc6d72381a7ae9
Mike Hoyle <hoylemd@gmail.com>
2023-12-01 10:03:30 -0500

    2023/1.1

--- 2023/1/main.py
+++ 2023/1/main.py
@@ -2,28 +2,44 @@ from argparse import ArgumentParser
 from utils import logger, parse_input
 
 
+def getInt(string: str):
+    try:
+        return int(string)
+    except ValueError:  # invalid literal for int
+        return None
+
+
 class Thing:
-    def __init__(self, *args: tuple, part=1, **kwargs: dict):
-        self.args = args
-        self.kwargs = kwargs
+    def __init__(self, numbers, part=1):
+        self.numbers = numbers
         self.part = part
 
     @classmethod
-    def parse_line(cls, line):
-        return (word for word in line.split())
+    def parse_line(cls, line, part=1):
+        """extract the first and last digits, combine them into a new number"""
+        li, ri = 0, -1
+        lval, rval = None, None
+
+        while (lval := getInt(line[li])) is None:
+            li += 1
+
+        while (rval := getInt(line[ri])) is None:
+            ri -= 1
+
+        return 10 * lval + rval
 
     @classmethod
     def parse(cls, lines, line_parser=None, part=1):
         line_parser = line_parser or cls.parse_line
         return cls(
-            (line_parser(line) for line in lines),
+            (line_parser(line, part) for line in lines),
             part=part)
 
     def __str__(self):
         return f"{self.__class__.__name__}(part {self.part})"
 
     def answer(self, *args, **kwargs):
-        return 0
+        return sum(self.numbers)
 
 
 arg_parser = ArgumentParser('python -m 2023.1.main', description="Advent of Code 2023 Day 1")
