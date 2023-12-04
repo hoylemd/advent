e5990d0b07575c73840cd1b03b83970cbaa738f8
Mike Hoyle <hoylemd@gmail.com>
2023-12-04 10:03:01 -0500

    2023/4 part 12 solution

--- 2023/4/main.py
+++ 2023/4/main.py
@@ -2,8 +2,24 @@ from argparse import ArgumentParser
 from utils import logger, parse_input
 
 
+def parse_card(card):
+    _, numbers = card.split(': ')
+    winning, have = numbers.split(' | ')
+    return [int(w) for w in winning.split()], [int(h) for h in have.split()]
+
+
+def score_card(winning, have):
+    logger.info(f"w: {winning}, h: {have}")
+    matches = set(winning).intersection(set(have))
+    score = 2 ** (len(matches) - 1) if matches else 0
+
+    logger.info(f"matches: {matches}, score: {score}")
+
+    return score
+
+
 def score_cards(cards):
-    pass
+    return sum(score_card(*parse_card(card)) for card in cards)
 
 
 arg_parser = ArgumentParser('python -m 2023.4.main', description="Advent of Code 2023 Day 4")
