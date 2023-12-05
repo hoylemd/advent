069fb004b7eddbbafc2446b2285625dea7c17d28
Mike Hoyle <hoylemd@gmail.com>
2023-12-05 12:34:06 -0500

    make start day script more functional focused

--- start_day.sh
+++ start_day.sh
@@ -4,10 +4,22 @@ year=$1
 shift
 day=$1
 shift
-class=$1
-shift
-obj=$1
-shift
+template='main.mustache'
+if [[ "$1" == "--OO" ]]; then
+  template='main_oo.mustache'
+  shift
+  class=$1
+  shift
+  obj=$1
+  shift
+else
+  p1_function=$1
+  shift
+  doc_name=$1
+  shift
+  p2_function=$1
+  shift
+fi
 
 mkdir -p $year/$day
 
@@ -19,16 +31,32 @@ if [[ ! "$obj" ]]; then
   obj='thing'
 fi
 
+if [[ ! "$p1_function" ]]; then
+  p1_function='answer_first_part'
+fi
+
+if [[ ! "$doc_name" ]]; then
+  doc_name='lines'
+fi
+
+if [[ ! "$p2_function" ]]; then
+  p2_function='answer_second_part'
+fi
+
 cat << EOF > temp_data.json
 {
   "year": $year,
   "day": $day,
   "class": "$class",
-  "obj": "$obj"
+  "obj": "$obj",
+  "part_1_function": "$p1_function",
+  "doc_name": "$doc_name",
+  "part_2_function": "$p2_function"
 }
 EOF
 
-mustache temp_data.json main.mustache > $year/$day/main.py
+
+mustache temp_data.json $template > $year/$day/main.py
 touch $year/$day/test.txt
 touch $year/$day/test2.txt
 touch $year/$day/input.txt
