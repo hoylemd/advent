e1d249998183974422630148441efc7d13b05cbe
Mike Hoyle <hoylemd@gmail.com>
2023-12-11 14:23:48 -0500

    2023/10 part 2

--- 2023/10/main.py
+++ 2023/10/main.py
@@ -30,6 +30,15 @@ PIPES = {
 }
 
 
+def figure_pipe_part(first_part_delta: Tuple[int, int], last_part_delta: Tuple[int, int]) -> str:
+    deltas = set([first_part_delta, last_part_delta])
+    for pipe, dirs in PIPES.items():
+        if deltas == set(DIRECTIONS[d] for d in dirs):
+            return pipe
+
+    raise ValueError('cant be done?')
+
+
 class NoPathError(Exception):
     pass
 
@@ -48,6 +57,7 @@ class PipeGrid:
 
         self.height = len(self.rows)
         self.width = len(self.rows[0])
+        self.loop_coords = {}
 
     def row_strings(self):
         return (''.join(row) for row in self.rows)
@@ -87,9 +97,14 @@ class PipeGrid:
         y, x = self.start
         ny, nx = y + DIRECTIONS[direction][0], x + DIRECTIONS[direction][1]
         steps = 1
+        self.loop_coords = {(y, x): self.rows[y][x]}
+        first_loop_part = None
 
         while (ny, nx) != self.start:
             connections = self.connecting_coords(ny, nx)
+            self.loop_coords[(ny, nx)] = self.rows[ny][nx]
+            if first_loop_part is None:
+                first_loop_part = (ny, nx)
 
             if connections[0] == (y, x):
                 y, x = ny, nx
@@ -101,13 +116,50 @@ class PipeGrid:
                 raise NoPathError('Current corrdinates are not connected to next pipe? hwat')
             steps += 1
 
+        start_part = figure_pipe_part(
+            (first_loop_part[0] - ny, first_loop_part[1] - nx),  # type: ignore
+            (y - ny, x - nx))  # type: ignore
+        self.loop_coords[(self.start[0], self.start[1])] = start_part
+
         return steps
 
 
-def answer2(grid: PipeGrid) -> int:
+def loop_enclosed_area(grid: PipeGrid) -> int:
     accumulator = 0
 
+    # find the loop
+    farthest_pipe_distance(grid)
+
     # solve part 2
+    for y, row in enumerate(grid.rows):
+        mode = 0
+        vis_row = []
+        h_seg_start = None
+
+        cross_corners = [set(['F', 'J']), set(['L', '7'])]
+        for x, cell in enumerate(row):
+            loop_pipe = grid.loop_coords.get((y, x))
+            if loop_pipe is None:
+                if mode:
+                    vis_row.append('#')
+                    accumulator += 1
+                else:
+                    vis_row.append('.')
+            else:
+                vis_row.append(loop_pipe)
+                if loop_pipe == '|':
+                    mode = (mode + 1) % 2
+                elif loop_pipe == '-':
+                    pass  # horizontal pipe, doesn't mean anything
+                else:
+                    if h_seg_start is None:
+                        h_seg_start = loop_pipe
+                    else:
+                        if set([h_seg_start, loop_pipe]) in cross_corners:
+                            mode = (mode + 1) % 2
+                        h_seg_start = None
+
+        logger.info(''.join(vis_row))
 
     return accumulator
 
@@ -137,7 +189,7 @@ if __name__ == '__main__':
     if argus.part == 1:
         answer = farthest_pipe_distance(grid)
     else:
-        answer = answer2(grid)
+        answer = loop_enclosed_area(grid)
 
     logger.debug('')
 
