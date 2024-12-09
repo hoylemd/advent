79b56f1c8dc8dae040a123c291ce04ac37895956
Mike Hoyle <hoylemd@gmail.com>
2024-12-09 15:20:17 -0500

    wow am i dumb today or what

--- utils.py
+++ utils.py
@@ -754,23 +754,15 @@ def find_cycle(seq: list[int]):
 
     return idx - r, length
 
+
+B64_REPS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+='
 def i_to_b64_chr(n: int) -> str:
     if n < 0 or n > 63:
         raise IndexError(f"Integer {n} can't be encoded to single-b64 char")
 
-    if n < 10: # 0-9, just int repr
-        return f"{n}"
-    elif n < 36: # 10-34: A-Z
-        return f"{chr((n - 10) + ord('A'))}"
-    elif n < 62:
-        return f"{chr((n - 36) + ord('a'))}"
-    elif n == 62:
-        return '+'
-    else: # n == 63
-        return '='
+    return B64_REPS[n]
 
 
-B64_REPS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+='
 def b64_chr_to_i(s: str) -> int:
     if len(s) != 1:
         raise ValueError(f"String '{s}' is not a single B64 char")
