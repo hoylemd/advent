fd48f046c93e7ac69e3b09b5b77ce8f2ebefccde
Mike Hoyle <hoylemd@gmail.com>
2024-12-10 16:55:42 -0500

    Add score labels

--- pong/pong.py
+++ pong/pong.py
@@ -1,7 +1,8 @@
 from batgrl.app import App
 from batgrl.colors import MAGENTA, GREEN
 from batgrl.gadgets.pane import Pane
-from batgrl.geometry.basic import Size
+from batgrl.gadgets.text import Text
+from batgrl.geometry.basic import Size, Point
 
 FIELD_DIMS = Size(25, 100)
 PADDLE_DIMS = Size(5, 1)
@@ -35,7 +36,7 @@ class Pong(App):
             up="w",
             down="s",
             size=PADDLE_DIMS,
-            pos=(center, 1),
+            pos=Point(center, 1),
             bg_color=GREEN
         )
 
@@ -43,11 +44,30 @@ class Pong(App):
             up="up",
             down="down",
             size=PADDLE_DIMS,
-            pos=(center, FIELD_DIMS.width - 2),
+            pos=Point(center, FIELD_DIMS.width - 2),
             bg_color=GREEN
         )
 
-        game_field.add_gadgets(left_paddle, right_paddle)
+        divider = Pane(
+            size=Size(1, 1),
+            size_hint={"height_hint": 1.0},
+            pos_hint={"x_hint": 0.5},
+            bg_color=GREEN
+        )
+
+        left_score_label = Text(
+            size=Size(1, 5),
+            pos=Point(1, 1),
+            pos_hint={"x_hint": 0.25}
+        )
+
+        right_score_label = Text(
+            size=Size(1, 5),
+            pos=Point(1, 1),
+            pos_hint={"x_hint": 0.75}
+        )
+
+        game_field.add_gadgets(left_paddle, right_paddle, divider, left_score_label, right_score_label)
         self.add_gadget(game_field)
 
 
