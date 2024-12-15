3003a64d880d5e33e77d992ff160aac7cb4df2c4
Mike Hoyle <hoylemd@gmail.com>
2024-12-14 21:39:56 -0500

    day 12 part 1

--- 2024/12/main.py
+++ 2024/12/main.py
@@ -4,99 +4,48 @@ from collections import defaultdict
 
 from utils import logger, parse_input, CharGrid, coordinates, INFINITY
 
-type stripe = tuple[str, int, int] # crop, offset, length
-
-
-def stripes_overlap(first: stripe, second: stripe) -> bool:
-    return calc_overlap(first, second) > 0
-
-def calc_overlap(first: stripe, second: stripe) -> int:
-    if first[1] > second[1]:
-        first, second = second, first
-
-    return first[1] + first[2] - second[1]
-
-
-
 
 class Region:
-    def __init__(self, first_y: int, first_stripe: stripe):
-        self.crop = first_stripe[0]
-        """
+
+    def __init__(self, parent: 'GardenMap', id: int, first_y: int, first_x: int):
+        self.parent = parent
+        self.id = id
+        self.crop = parent.get(first_y, first_x)
         self.y = first_y
-        self.x = first_stripe[1]
+        self.x = first_x
         self.height = 1
-        self.width = first_stripe[2]
-        """
-        self.stripes = defaultdict(list[stripe])
+        self.width = 1
+        self.points: set[coordinates] = set()
+        self.edges: set[coordinates] = set()
+        self.ext_corners: set[coordinates] = set()
+        self.int_corners: set[coordinates] = set()
+        self.adjacency = defaultdict(set)
 
-        self.add_stripe(first_y, first_stripe)
+    @property
+    def right(self):
+        return self.x + self.width
 
-    def _add_stripe(self, y: int, next_stripe: stripe):
-        c, x, l = next_stripe
+    @property
+    def bottom(self):
+        return self.y + self.height
 
-        if c != self.crop:
-            raise ValueError('Cannot add stripe of different crop')
+    def add_point(self, y: int, x: int):
+        self.points.add((y, x))
 
-        """
-        if (delta_y := y - self.y) < 0:
+        if y < self.y:
+            self.height += self.y - y
             self.y = y
-            self.height += delta_y
 
-        if (delta_x := x - self.x) < 0:
-            self.x
-        """
-        current_row = self.stripes[y]
-        current_row.append(next_stripe)
+        if y > self.bottom:
+            self.height += y - self.bottom
 
-        self.stripes[y] = sorted(current_row, key=lambda s: s[1])
+        if x < self.x:
+            self.width += self.x - x
 
-    def add_stripe(self, y: int, next_stripe: stripe):
-        self._add_stripe(y, next_stripe)
-        self.compute_bounds()
+        if x > self.bottom:
+            self.width += x - self.bottom
 
-    def add_stripes(self, stripes: Mapping[int, list[stripe]]):
-        for y, y_stripes in stripes.items():
-            for s in y_stripes:
-                self._add_stripe(y, s)
-
-        self.compute_bounds()
-
-    def compute_bounds(self):
-        ys = sorted(self.stripes.keys())
-
-        self.y = ys[0]
-        self.height = len(ys)
-
-        self.x = INFINITY
-        right = 0
-        for y in ys:
-            for s in self.stripes[y]:
-                s_right = s[1] + s[2]
-
-                if self.x is None:
-                    self.x = s[1]
-
-                if s[1] < self.x:
-                    # if new stripe offset is to the left
-                    self.x = s[1]
-
-                if s_right > right:
-                    # if new right is to the right
-                    right = s_right
-
-        self.width = right - self.x
-
-    def can_merge(self, other_y: int, other: stripe) -> bool:
-        if other[0] != self.crop:
-            # wrong crop
-            return False
-
-        for b_stripe in self.stripes[other_y -1]:
-            if stripes_overlap(b_stripe, other):
-                return True
-
-        return False
+        self.parent.region_map[y][x] = self
 
     def __str__(self) -> str:
         return f"Region of {self.crop} from {self.y}, {self.x} to {self.y + self.height - 1}, {self.x + self.width - 1}"
@@ -104,30 +53,64 @@ class Region:
     def __repr__(self) -> str:
         return f"{self}"
 
-    def render_row(self, y: int, full_width: int) -> str:
-        buffer = ''
-        return buffer
+    def map_region(self):
+        """starting at y,x, find all adjacent cells with the same crop, count permimeters"""
+        checked: set[coordinates] = set()
+        to_check: list[coordinates] = [(self.y, self.x)]
 
+        while len(to_check):
+            y, x = to_check.pop()
+            if (y, x) in checked:
+                continue
 
-    def render(self, full_height: int, full_width: int) -> str:
-        return '\n'.join(
-            self.render_row(y, full_width)
-            for y in range(full_height)
-        )
+            next_cells = self.map_cell(y, x)
+            logger.debug(f"Adding {y, x} to Region {self.id}({self.crop})")
+            self.add_point(y, x)
+
+            checked.add((y, x))
+            to_check += next_cells
+
+    def map_cell(self, y: int, x: int) -> list[coordinates]:
+        """for a given cell get adjacent same-region cells & determine if this is an edge piece"""
+
+        next_cells = []
+        for ay, ax in self.parent.get_adjacent_coordinates(y, x):
+            if self.parent.lines[ay][ax] != self.crop:
+                continue
+
+            if self.parent.region_map[ay][ax] is None:
+                next_cells.append((ay, ax))
+
+            self.adjacency[(y, x)].add((ay, ax))
+
+        if len(next_cells) < 4:
+            self.edges.add((y, x))
+
+        return next_cells
+
+    def count_perimeter(self) -> int:
+        p = 0
+
+        for edge in self.edges:
+            p += 4 - len(self.adjacency[edge])
+
+        return p
 
     def price(self) -> int:
-        area = 0
-        perimeter = sum(s[2] for s in self.stripes[self.y]) # top edge
-        prev_row = []
+        return len(self.points) * self.count_perimeter()
 
-        for y, stripes in self.stripes.items():
-            perimeter += 2 * len(stripes) # sides
-            # top edge(s)
-            for p_r_stripe in self.stripes[y - 1]:
+    def render_cell(self, y: int, x: int) -> str:
+        assert self.crop is not None
 
-        return area * perimeter
+        if self.parent.region_map[y][x] == self:
+            return self.crop
+        return '.'
 
+    def render_line(self, y: int) -> str:
+        return ''.join(self.render_cell(y, x) for x in range(self.x, self.right + 1))
 
+    def render(self) -> str:
+        return '\n'.join(self.render_line(y) for y in range(self.y, self.bottom + 1))
 
 
 class GardenMap(CharGrid):
@@ -135,75 +118,21 @@ class GardenMap(CharGrid):
     def __init__(self, lines: Iterator[str], part: int = 1):
         self.part = part
 
-        self.rows = list(self.parse_lines(lines))
+        super().__init__(lines)
+        self.regions = []
+        self.region_map: list[list[Region | None]] = [[None] * self.width for y in range(self.height)]
 
-        self.regions = self.merge_regions()
+        self.map_regions()
 
-    def __str__(self) -> str:
-        return f"{self.__class__.__name__}(part {self.part})"
+    def map_regions(self):
+        for y, line in enumerate(self.lines):
+            for x, cell in enumerate(line):
+                if self.region_map[y][x] is None:
+                    new_region = Region(self, len(self.regions), y, x)
+                    self.region_map[y][x] = new_region
+                    self.regions.append(new_region)
+                    new_region.map_region()
 
-    def parse_line(self, y: int, line: str) -> Iterator[stripe]:
-        current_crop = ''
-        current_offset = 0
-
-        for i, c in enumerate(line):
-            if c != current_crop:
-                # new stripe
-                if current_crop:
-                    yield (current_crop, current_offset, i - current_offset)
-
-                current_offset = i
-                current_crop = c
-
-        yield (current_crop, current_offset, i - current_offset + 1)
-
-
-    def parse_lines(self, lines: Iterator[str]) -> Iterator[list[stripe]]:
-        for y, line in enumerate(lines):
-            yield list(self.parse_line(y, line))
-
-    def esrap_line(self, y: int, row: list[stripe]) -> str:
-        return ''.join(f"{c * l}" for c, _, l in row)
-
-    def esrap_lines(self) -> str:
-        return '\n'.join(
-            self.esrap_line(y, r) for y, r in enumerate(self.rows)
-        )
-
-    def print_stripes(self) -> str:
-        return '\n'.join(f"{row}" for row in self.rows)
-
-    def merge_regions(self) -> list[Region]:
-        all_regions: list[Region] = []
-        prev_row_regions: list[Region] = []
-        this_row_regions: list[Region] = []
-        for y, row in enumerate(self.rows):
-            for i, stripe in enumerate(row):
-                this_region = None
-                for region in prev_row_regions:
-
-                    # if stripe[0] == 'V' and y == 5:
-                    #     breakpoint()
-
-                    if region.can_merge(y, stripe):
-                        if this_region is None:
-                            this_region = region
-                            region.add_stripe(y, stripe)
-                        else:
-                            this_region.add_stripes(region.stripes)
-                            all_regions.remove(region)
-
-                if this_region is None:
-                    this_region = Region(y, stripe)
-                    all_regions.append(this_region)
-
-                if this_region not in this_row_regions:
-                    this_row_regions.append(this_region)
-
-            prev_row_regions = this_row_regions
-            this_row_regions = []
-
-        return all_regions
 
 def answer2(garden: GardenMap) -> int:
     accumulator = 0
@@ -213,12 +142,10 @@ def answer2(garden: GardenMap) -> int:
     return accumulator
 
 
-def answer1(garden: GardenMap) -> int:
+def fence_prices(garden: GardenMap) -> int:
     accumulator = 0
-
-    logger.info(garden.print_stripes())
-    breakpoint()
-    # solve part 1
+    for region in garden.regions:
+        accumulator += region.price()
 
     return accumulator
 
@@ -236,7 +163,7 @@ if __name__ == '__main__':
         case -1:
             answer = garden.esrap_lines()
         case 1:
-            answer = answer1(garden)
+            answer = fence_prices(garden)
         case 2:
             answer = answer2(garden)
 
