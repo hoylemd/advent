43fefd7f2159406d14ba2a9bc4dc8542e4582531
Mike Hoyle <hoylemd@gmail.com>
2024-12-17 09:07:02 -0500

    day 16 part 2 (incomplete)

--- 2024/16/main.py
+++ 2024/16/main.py
@@ -38,6 +38,10 @@ class ReindeerMaze(CharGrid):
                 shortest = dist
                 s_dir = dir
 
+        if (sy, sx) == (-1, -1):
+            # no more reachable nodes
+            raise StopIteration
+
         self.unvisited.remove((sy, sx))
 
         return (sy, sx), shortest, s_dir
@@ -51,14 +55,17 @@ class ReindeerMaze(CharGrid):
             return self.lines[y][x] != '#' and (y, x) in self.unvisited
 
         while(len(self.unvisited)): # technically should also check for all-infinity, but that wont happen
-            (y, x), distance, (hy, hx) = self.pop_shortest_unvisited()
-            logger.info(f"At {y, x}, current distance is {distance}, heading {hy, hx}")
+            try:
+                (y, x), distance, (hy, hx) = self.pop_shortest_unvisited()
+            except StopIteration:
+                break
+            #logger.info(f"At {y, x}, current distance is {distance}, heading {hy, hx}")
 
             if distance == INFINITY:
                 # shortest unvisited is unreachable what
                 breakpoint()
 
-            if (y, x) == self.end:
+            if (y, x) == self.end and self.part == 1:
                 return distance # shortest path found! (probably)
 
             for cy, cx in self.get_adjacent_coordinates(y, x, test=is_open_and_unvisited):
@@ -66,15 +73,60 @@ class ReindeerMaze(CharGrid):
                 if (cy, cx) != (y + hy, x + hx):
                     cost_from_here += 1000
 
-                logger.info(f"checking {cy, cx}: {cost_from_here}")
+                #logger.info(f"checking {cy, cx}: {cost_from_here}")
 
                 if cost_from_here < self.nav_map[cy][cx][0]:
-                    logger.info(f"  shorter than {self.nav_map[cy][cx][0]}")
+                    #logger.info(f"  shorter than {self.nav_map[cy][cx][0]}")
                     self.nav_map[cy][cx] = (cost_from_here, (cy - y, cx - x))
 
 
-        breakpoint()
         return distance
+
+    def count_best_paths(self) -> int:
+        best_path_tiles = set()
+
+        self.djikstra()
+
+        def is_reachable(y: int, x: int) -> bool:
+            return self.nav_map[y][x][0] < INFINITY
+
+        def by_distance(coords) -> int:
+            y, x = coords
+            return self.nav_map[y][x][0]
+
+        # backtrack from end?
+        to_check = [self.end]
+
+
+        while len(to_check):
+            y, x = to_check.pop()
+
+            best_path_tiles.add((y, x))
+            logger.info(self.esrap_lines({tile: 'O' for tile in best_path_tiles}))
+
+            if (y, x) == self.start:
+                break
+
+            candidates = sorted(
+                self.get_adjacent_coordinates(y, x, test=is_reachable),
+                key=by_distance
+            )
+            if (y, x) == (7, 5):
+                breakpoint()
+
+            shortest = INFINITY
+            for cy, cx in candidates:
+                dist = self.nav_map[cy][cx][0]
+                if dist < shortest:
+                    shortest = dist
+                if dist > shortest:
+                    break
+
+                to_check.append((cy, cx))
+
+
+        return len(best_path_tiles)
+
 
 
 
@@ -101,7 +153,7 @@ if __name__ == '__main__':
         case 1:
             answer = maze.djikstra()
         case 2:
-            answer = answer2(maze)
+            answer = maze.count_best_paths()
 
     logger.debug('')
 
