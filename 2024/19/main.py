db2401414730f123f2b517dff47f84fdc7aa50b9
Mike Hoyle <hoylemd@gmail.com>
2024-12-19 11:34:53 -0500

    start on a more intelligent validation mechanism

--- 2024/19/main.py
+++ 2024/19/main.py
@@ -1,6 +1,6 @@
 from argparse import ArgumentParser
 from utils import logger, parse_input
-from typing import Iterator
+from typing import Iterator, Mapping
 
 
 class LinenLayout:
@@ -9,6 +9,26 @@ class LinenLayout:
         self.part = part
 
         self.towels, self.designs = self.parse_lines(lines)
+
+        self.sorted_towels = sorted(self.towels, key = lambda t: -1* len(t))
+        self.towels_by_width = {}
+        self.towels_by_width = self.towel_width_map()
+        self.impossible_patterns = []
+
+    def towel_width_map(self) -> Mapping[int, list[str]]:
+        last_width = None
+        reversed_towels = list(reversed(self.sorted_towels))
+        width_map = {}
+        for i, towel in enumerate(reversed_towels):
+            if last_width is None:
+                last_width = len(towel)
+
+            if len(towel) > last_width:
+                width_map[last_width] = list(reversed(reversed_towels[:i]))
+                last_width = len(towel)
+
+        return width_map
+
 
     def __str__(self) -> str:
         return f"{self.__class__.__name__}(part {self.part})"
@@ -37,6 +57,9 @@ class LinenLayout:
         ] + self.designs)
 
     def validate_design(self, design: str) -> bool:
+        pass
+
+    def slow_validate_design(self, design: str) -> bool:
         if len(design) == 0:
             return True
 
