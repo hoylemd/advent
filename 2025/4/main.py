19b9c388d15758cabc0ee95e7bdee30ce438016e
Mike Hoyle <hoylemd@gmail.com>
2025-12-07 16:13:41 -0500

    solde d4p1

--- 2025/4/main.py
+++ 2025/4/main.py
@@ -12,7 +12,20 @@ def pass_through(x: int, y: int, value: CellContent) -> str:
     return value
 
 
+DIRECTIONS = [
+    (-1, 0),  # up/north
+    (-1, 1),  # up-right/northeast
+    (0, 1),  # right/east
+    (1, 1),  # down-right/southeast
+    (1, 0),  # down/south
+    (1, -1),  # down-left/southwest
+    (0, -1),  # left/west
+    (-1, -1),  # up-left/northwest
+]
+
+
 class Map:
+    """Assumes all lines have same length"""
     def __init__(self, lines: Iterator[str], part: int = 1):
         self.part = part
 
@@ -57,8 +70,84 @@ class Map:
             for y, row in enumerate(self.rows)
         )
 
+    @property
+    def min_y(self):
+        return 0
 
-def answer2(map: Map, **_: dict) -> int:
+    @property
+    def min_x(self):
+        return 0
+
+    @property
+    def max_y(self):
+        return len(self.rows) - 1
+
+    @property
+    def max_x(self):
+        return len(self.rows[0]) - 1
+
+    def in_bounds(self, y: int, x: int) -> bool:
+        return (
+            (y >= self.min_y and y <= self.max_y) and
+            (x >= self.min_x and x <= self.max_x)
+        )
+
+
+class RollMap(Map):
+    def __init__(self, *args, **kwargs):
+        self.rolls = set()
+        self.accessible = set()
+
+        return super().__init__(*args, **kwargs)
+
+    def parse_cell(self, y: int, x: int, cell: str) -> CellContent:
+        if cell == '@':
+            self.rolls.add((y, x))
+
+        return cell
+
+    def parse_line(self, y: int, line: str) -> list[CellContent]:
+        return [self.parse_cell(y, x, cell) for x, cell in enumerate(line)]
+
+    def has_roll(self, y: int, x: int) -> bool:
+        return (y, x) in self.rolls
+
+    def render(self) -> str:
+        def accessible_shader(y: int, x: int, value: CellContent) -> str:
+            if (y, x) in self.accessible:
+                return 'x'
+            return value
+
+        return super().render(shader=accessible_shader)
+
+    def adjacent_cells(self, y: int, x: int) -> Iterator[tuple[int, int]]:
+        assert self.in_bounds(y, x), f"Coordinates {(y, x)} are out of bounds"
+
+        for dy, dx in DIRECTIONS:
+            cy, cx = y + dy, x + dx
+            if not self.in_bounds(cy, cx):
+                continue
+            yield (cy, cx)
+
+    def adjacent_rolls(self, y: int, x: int) -> list[tuple[int, int]]:
+        # return [(cy, cx) for cy, cx in self.adjacent_cells(y, x) if ((cy, cx) in self.rolls)]
+        adj_rolls = []
+        for cy, cx in self.adjacent_cells(y, x):
+            if (cy, cx) in self.rolls:
+                adj_rolls.append((cy, cx))
+        return adj_rolls
+
+    def count_adjacent_rolls(self, y: int, x: int) -> int:
+        return len(self.adjacent_rolls(y, x))
+
+    def find_accessible(self) -> set[tuple[int, int]]:
+        return set(
+            (y, x) for (y, x) in self.rolls
+            if self.count_adjacent_rolls(y, x) < 4
+        )
+
+
+def answer2(map: RollMap, **_: dict) -> int:
     accumulator = 0
 
     # solve part 2
@@ -66,28 +155,14 @@ def answer2(map: Map, **_: dict) -> int:
     return accumulator
 
 
-def answer1(map: Map, **_: dict) -> int:
-    accumulator = 0
+def answer1(map: RollMap, **_: dict) -> int:
 
-    known_accessible = set([
-        (0, 2), (0, 3), (0, 5), (0, 6), (0, 8),
-        (1, 0),
-        (2, 6),
-        (4, 0), (4, 9),
-        (7, 0),
-        (9, 0), (9, 2), (9, 8)
-    ])
-
-    def accessible_shader(y: int, x: int, value: CellContent) -> str:
-        if (y, x) in known_accessible:
-            return 'x'
-        return value
-
-    print(map.render(shader=accessible_shader))
+    map.accessible = map.find_accessible()
+    logger.debug(map.render())
 
     # solve part 1
 
-    return accumulator
+    return len(map.accessible)
 
 
 INPUT_PARAMS = {
@@ -109,7 +184,7 @@ if __name__ == '__main__':
 
     params = INPUT_PARAMS[os.path.basename(argus.input_path)]
     lines = parse_input(argus.input_path)
-    map = Map(lines, part=argus.part)
+    map = RollMap(lines, part=argus.part)
     match argus.part:
         case -1:
             answer = map.esrap_lines()
